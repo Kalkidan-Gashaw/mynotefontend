f80cde1b3c4113dbb3c41e4a2a14ff113d7c4a5d
Kalkidan-Gashaw <162141690+Kalkidan-Gashaw@users.noreply.github.com>
2025-02-17 17:17:34 +0300

    Update Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,30 +1,30 @@
-import React, { useState } from "react";
-import ProfileInfo from "../Cards/profileInfo.jsx";
-import { useNavigate } from "react-router-dom";
-import SearchBar from "../SearchBar/SearchBar.jsx";
+import React, { useState } from 'react';
+import ProfileInfo from '../Cards/ProfileInfo.jsx'; // Verify this path
+import { useNavigate } from 'react-router-dom';
+import SearchBar from '../SearchBar/SearchBar';
 
 const Navbar = ({ userInfo, onSearchNote, handleClearSearch }) => {
-  const [searchQuery, setSearchQuery] = useState("");
+  const [searchQuery, setSearchQuery] = useState('');
   const navigate = useNavigate();
 
   const onLogout = () => {
     localStorage.clear();
-    navigate("/");
+    navigate('/');
   };
 
-  const handelSearch = () => {
+  const handleSearch = () => {
     if (searchQuery) {
       onSearchNote(searchQuery);
     }
   };
 
   const onClearSearch = () => {
-    setSearchQuery("");
+    setSearchQuery('');
     handleClearSearch(); // Ensure it calls the correct function
   };
 
   return (
-    <div className="navc flex items-center justify-between px-6 py-2 ">
+    <div className="navc flex items-center justify-between px-6 py-2">
       <h2 className="logo">
         My<span className="spanN">Notes</span>
       </h2>
@@ -33,7 +33,7 @@ const Navbar = ({ userInfo, onSearchNote, handleClearSearch }) => {
         onChange={({ target }) => {
           setSearchQuery(target.value);
         }}
-        handelSearch={handelSearch}
+        handleSearch={handleSearch} // Update function name here
         onClearSearch={onClearSearch}
       />
       <ProfileInfo userInfo={userInfo} onLogout={onLogout} />
